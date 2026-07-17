c9436c51413ab4d2d5150fb57d0776ecc92d0d69
Tricia Miller <eagleswings913@gmail.com>
2026-07-17 16:28:57 -0400

    put game images in a list, change location of error checking

--- main.py
+++ main.py
@@ -33,44 +33,41 @@ scissors = '''
 ---.__(___)
 '''
 
-def print_choice(choice):
-    if choice == 0:
-        print(rock)
-    elif choice == 1:
-        print(paper)
-    else:
-        print(scissors)
+game_images = [rock, paper, scissors]
 
 def random_number():
     return random.randint(0, 2)
 
 user_choice = int(input('What do you choose? Type 0 for Rock, 1 for Paper, 2 for Scissors: \n'))
-print_choice(user_choice)
 
-computer_choice = random_number()
-print ('Computer chose: ', computer_choice)
-print_choice(computer_choice)
+if 0 <= user_choice <= 2:
 
-if user_choice == 0:
-    if computer_choice == 0:
-        print("Tie")
-    elif computer_choice == 1:
-        print ("Computer wins")
-    else:
-        print ("You win")
-elif user_choice == 1:
-    if computer_choice == 0:
-        print("You win!")
-    elif computer_choice == 1:
-        print("Tie")
-    else:
-        print("Computer wins!")
-elif user_choice == 2:
-    if computer_choice == 0:
-        print("Computer wins!")
-    elif computer_choice == 1:
-        print("You win!")
-    else:
-        print("Tie")
+    print(game_images[user_choice])
+
+    computer_choice = random_number()
+    print ('Computer chose: ', computer_choice)
+    print(game_images[computer_choice])
+
+    if user_choice == 0:
+        if computer_choice == 0:
+            print("Tie")
+        elif computer_choice == 1:
+            print ("Computer wins")
+        else:
+            print ("You win")
+    elif user_choice == 1:
+        if computer_choice == 0:
+            print("You win!")
+        elif computer_choice == 1:
+            print("Tie")
+        else:
+            print("Computer wins!")
+    elif user_choice == 2:
+        if computer_choice == 0:
+            print("Computer wins!")
+        elif computer_choice == 1:
+            print("You win!")
+        else:
+            print("Tie")
 else:
     print("Invalid choice: Please enter 0 for Rock, 1 for Paper, 2 for Scissors")
