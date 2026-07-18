f52f7398e7649bd953c93b641fbe8e5b08e4a860
Tricia Miller <eagleswings913@gmail.com>
2026-07-18 09:33:53 -0400

    Added tracking games stats

--- main.py
+++ main.py
@@ -35,12 +35,16 @@ scissors = '''
 
 game_images = [rock, paper, scissors]
 
+game_stats = {"games": 0, "ties": 0, "user_wins": 0, "computer_wins": 0}
+
 continue_play = True
 
 def random_number():
     return random.randint(0, 2)
 
 while continue_play:
+    game_stats["games"] += 1
+
     user_choice = int(input('What do you choose? Type 0 for Rock, 1 for Paper, 2 for Scissors: \n'))
 
     if 0 <= user_choice <= 2:
@@ -53,24 +57,36 @@ while continue_play:
 
         if user_choice == computer_choice:
             print("It's a draw!")
+            game_stats["ties"] += 1
         else:
             if user_choice == 0:
                 if computer_choice == 1:
                     print ("Computer wins")
+                    game_stats["computer_wins"] += 1
                 else:
                     print ("You win")
+                    game_stats["user_wins"] += 1
             elif user_choice == 1:
                 if computer_choice == 0:
                     print("You win!")
+                    game_stats["user_wins"] += 1
                 else:
                     print("Computer wins!")
+                    game_stats["computer_wins"] += 1
             elif user_choice == 2:
                 if computer_choice == 0:
                     print("Computer wins!")
+                    game_stats["computer_wins"] += 1
                 else:
                     print("You win!")
+                    game_stats["user_wins"] += 1
     else:
         print("Invalid choice: Please enter 0 for Rock, 1 for Paper, 2 for Scissors")
 
     if input('Do you want to play again? (y/n): ').lower() == 'n':
         continue_play = False
+
+print(f"You played {game_stats['games']} times!")
+print(f"You tied {game_stats['ties']} times!")
+print(f"You won {game_stats['user_wins']} times!")
+print(f"Computer won {game_stats['computer_wins']} times!")
